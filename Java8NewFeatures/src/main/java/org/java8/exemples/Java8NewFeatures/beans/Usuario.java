a82f90c0e61952277065aace82b821e30a249dea
rafael.carmo <rafael.carmo@CAMSTIDIS1177H.fazenda.mg>
2018-04-18 14:35:06 -0300

    teste com o párca.

--- Java8NewFeatures/src/main/java/org/java8/exemples/Java8NewFeatures/beans/Usuario.java
+++ Java8NewFeatures/src/main/java/org/java8/exemples/Java8NewFeatures/beans/Usuario.java
@@ -1,9 +1,15 @@
 package org.java8.exemples.Java8NewFeatures.beans;
 
+/**
+ * testando novamente
+ * @author rafael.carmo
+ *
+ */
 public class Usuario {
 	private String name;
 	private int points;
 	private boolean moderator;
+	private int numero;
 
 	public Usuario(String name, int points) {
 		this.points = points;
